e9058df6b76d943cf3a2678a9615febcb372a466
Hamza Munir <nnaz55571@gmail.com>
2025-09-05 02:53:11 +0000

    Error: Invalid src prop (https://onvurqhacnjbgajtbokn.supabase.co/storag

--- next.config.ts
+++ next.config.ts
@@ -23,6 +23,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+       {
+        protocol: 'https',
+        hostname: 'onvurqhacnjbgajtbokn.supabase.co',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 };
