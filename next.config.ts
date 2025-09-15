13e0397bd297081b363fb65e0070cac836ce86f3
Hamza Munir <nnaz55571@gmail.com>
2025-09-15 03:39:56 +0000

    Diagnosis: In the build logs, the error message "require.extensions is n

--- next.config.ts
+++ next.config.ts
@@ -31,6 +31,12 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push('handlebars');
+    }
+    return config;
+  }
 };
 
 export default nextConfig;
