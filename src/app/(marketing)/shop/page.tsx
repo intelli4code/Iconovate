26d55ae05a8b94c29fd4101e0501c675426d1303
Hamza Munir <nnaz55571@gmail.com>
2025-09-15 03:22:01 +0000

    Ok buddy i waas deloying this site to netlify but it is saying this Net

--- src/app/(marketing)/shop/page.tsx
+++ src/app/(marketing)/shop/page.tsx
@@ -1,4 +1,4 @@
-import ShopPageContent from "./shop-page-content";
+import ShopPageContent from "../shop-page-content";
 
 export default async function ShopPage() {
     return <ShopPageContent />;
